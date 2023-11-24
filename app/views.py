41f0a4c84c137b6259c1dedb49130c43f2267706
yaswanth <yashwanthreddyramireddy@gmail.com>
2023-11-24 13:55:09 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def leo(request):
+    return render(request,'leo.html')
+def bigil(request):
+    return render(request,'bigil.html')
